c8bbbfb57ef80120adb0fda1eab409a8cfecd5e5
Yoram Michaeli <yorammi@yorammi.com>
2023-02-10 20:35:36 +0200

    add stages loop

--- src/yorammi/ez/ezEasy.groovy
+++ src/yorammi/ez/ezEasy.groovy
@@ -28,7 +28,7 @@ class ezEasy extends ezBaseJob {
                 script.ezLog.anchor "${stage.name}"
                 script.stage("${stage.name}") {
                     stage.steps.each { step ->
-                        eval "${step}"
+                        script.eval "${step}"
                     }
                 }
             }
