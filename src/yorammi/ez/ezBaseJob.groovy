66cbb259644f400edb4cc547f8bd151c9bbf3c8d
Yoram Michaeli <yorammi@yorammi.com>
2023-02-10 17:15:30 +0200

    add classes

---
+++ src/yorammi/ez/ezBaseJob.groovy
@@ -0,0 +1,35 @@
+package yorammi.ez;
+
+abstract class ezBaseJob implements Serializable {
+
+    def script
+    def currentStage
+
+    ezBaseJob(script) {
+        this.script = script
+    }
+    
+    void activate() {
+        script.timestamps() {
+            try {
+                script.echo "Activating with params: ${this.properties}"
+                activateImpl()
+            } finally {
+                try {
+                } catch (all) {
+                }
+
+            }
+        }
+    }
+
+    void activateImpl() {
+    }
+
+    void activateStage(String name, Closure stage) {
+        script.ezLog.anchor("${name} stage start")
+        currentStage = name
+        script.stage(name, stage)
+        script.log.anchor("${name} stage end")
+    }
+}
---
+++ src/yorammi/ez/ezEasy.groovy
@@ -0,0 +1,42 @@
+package yorammi.ez;
+
+import yorammi.ez.baseJob
+import java.text.SimpleDateFormat 
+import java.util.Date
+
+class ezEasy extends ezBaseJob {
+
+    // internal class attributes
+    def buildNumber
+    def deleteWorkspace = true
+    def componentBranch = ''
+    Map config
+
+    // constructor
+    ezEasy(script) {
+        super(script)
+    }
+
+    @Override
+    void activateImpl() {
+        try {
+            buildNumber = script.env.BUILD_NUMBER
+            activateStage('Setup', this.&setup)
+        } catch (error) {
+            script.ezLog.debug "[ERROR] "+error.message
+            script.currentBuild.result = "FAILURE"
+        }
+        finally {
+        }
+    }
+
+    void setup() {
+        if(deleteWorkspace) {
+            script.checkout script.scm
+        }
+
+        script.sh "env"
+    }
+}
+
+
