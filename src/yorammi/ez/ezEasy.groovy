b2c2c65423dd34ea2e4dc9824196d203cdf11071
Yoram Michaeli <yorammi@yorammi.com>
2023-02-11 19:28:26 +0200

    add activatePhase

--- src/yorammi/ez/ezEasy.groovy
+++ src/yorammi/ez/ezEasy.groovy
@@ -47,8 +47,7 @@ class ezEasy extends ezBaseJob {
         def stages = phase.stages
         stages.each { stage ->
             yaml.stages.each { loopStage ->
-                script.echo "${loopStage.name} ?? ${stage}"
-                if("${loopStage.name}" == "${stage}") {
+                if(loopStage.name == stage) {
                     activateStage(loopStage)
                 }
             }
