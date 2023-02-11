4ff057ba8005092c9fa15f9a6103be1bf3b6ef15
Yoram Michaeli <yorammi@yorammi.com>
2023-02-11 04:41:49 +0200

    implement steps

--- src/yorammi/ez/ezEasy.groovy
+++ src/yorammi/ez/ezEasy.groovy
@@ -37,7 +37,7 @@ class ezEasy extends ezBaseJob {
                             script.echo step.args
                             break
                         case "step":
-                            "${step.args}"
+                            eval ("${step.args}")
                             break
                         default:
                             echo "Invalid step type"
