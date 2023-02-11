1a2702e007ee6a192956062bbbf3f6420cb78c80
Yoram Michaeli <yorammi@yorammi.com>
2023-02-11 20:08:08 +0200

    add activatePhase

--- vars/ezPipeline.groovy
+++ vars/ezPipeline.groovy
@@ -22,7 +22,7 @@ def call(Map config) {
     }
     String agentString = any
     if (config.ezMainLabel != null) {
-        agentString "{ label \"${config.ezMainLabel}\" }"
+        agentString = "{ label \"${config.ezMainLabel}\" }"
     }
 
     pipeline {
