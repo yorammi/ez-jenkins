abb60296d2fe315526be69ea1a08b0a0a0e449de
Yoram Michaeli <yorammi@yorammi.com>
2023-02-20 14:09:56 +0200

    add env setup

--- src/yorammi/ez/ezEasy.groovy
+++ src/yorammi/ez/ezEasy.groovy
@@ -73,8 +73,7 @@ class ezEasy extends ezBaseJob {
             script.ezLog.info "No flow environment variables"
         }
         def branch = script.env.BRANCH_NAME
-        script.currentBuild.displayName += " {branch:${branch}}}"
-        script.ezLog.debug "branch=${branch}"
+        script.currentBuild.displayName += " {branch:${branch}}"
         try {
             if("${script.env.BRANCH_IS_PRIMARY}"=="true") {
                 branchIsMandatory=true
--- vars/ezEnvSetup.groovy
+++ vars/ezEnvSetup.groovy
@@ -14,9 +14,9 @@ def initEnv(Map config=null) {
     }
     initUserEnvVars()
     if(ezUtils.isBuildStartedByUser() || env.BUILD_USER != '') {
-        if(env.BUILD_USER != "admin") {
-            currentBuild.displayName += " {${env.BUILD_USER}}"
-        }
+        // if(env.BUILD_USER != "admin") {
+            currentBuild.displayName += " {user:${env.BUILD_USER}}"
+        // }
     }
 }
 
